ce786189882995283c63186f5b2d82750a25e940
BB-choi <choibobae0809@gmail.com>
2022-05-10 09:25:35 +0900

    feat: eslint-plugin-import 적용
    - 절대경로로 jsx import시 external modules로 인식(internal로 안됨)
      - App.jsx에서 상대경로로 지정

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import NotFound from "components/NotFound/NotFound";
-import Layout from "Layout/Layout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import menus from "stores/menus";
-import Normalize from "Styles/Normalize";
-import Reset from "Styles/Reset";
-import VendingMachine from "views/VendingMachine/VendingMachine";
-import Wallet from "views/Wallet/Wallet";
+
+import NotFound from "./components/NotFound/NotFound";
+import Layout from "./Layout/Layout";
+import menus from "./stores/menus";
+import Normalize from "./styles/Normalize";
+import Reset from "./styles/Reset";
+import VendingMachine from "./views/VendingMachine/VendingMachine";
+import Wallet from "./views/Wallet/Wallet";
 
 const App = () => {
   return (
