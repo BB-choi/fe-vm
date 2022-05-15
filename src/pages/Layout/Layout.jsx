aa658f09fcf18cb14b2a21a10c5b87a987b0d9cc
BB-choi <choibobae0809@gmail.com>
2022-05-15 11:22:02 +0900

    comment: TODO, NOTE 주석 추가

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -6,11 +6,16 @@ import { Outlet } from "react-router-dom";
 
 import { Wrap, Main } from "./Layout.styled";
 
+// TODO: context 분리하기
 export const MoneyContext = React.createContext({});
 export const setMoneyContext = React.createContext(() => {});
 
 const Layout = ({ menusData }) => {
+  // NOTE: 더 좋은 방법이 없는지..?
   const [cashData, setCashData] = useState(cash);
+
+  // NOTE: useMemo, useCallback사용에도 re-rendering이 되는 곳
+  // (lint설정으로 인해 사용해야하기때문에 일단 놔두기😭)
   const decreaseCashCount = useCallback(
     (indexNo) =>
       setCashData(
--- src/pages/Wallet/Wallet.jsx
+++ src/pages/Wallet/Wallet.jsx
@@ -12,6 +12,8 @@ const Wallet = () => {
 
   const walletItems = cashData.map(({ money }, idx) => {
     return (
+      // NOTE: 여기서 context의 value를 해당 MoneyItem에 해당하는 것으로 지정하여
+      // 리렌더링을 막아보려 했으나 onClick이벤트에 의해서 전체가 리렌더링 되게 됨🤔
       <MoneyContext.Provider value={cashData[idx]} key={money}>
         <MoneyItem onClick={() => handleClickButton(idx)} />
       </MoneyContext.Provider>
