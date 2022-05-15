54bc10671c3b30d48cbebb6e277dbf97f015ec26
BB-choi <choibobae0809@gmail.com>
2022-05-15 11:10:02 +0900

    remove: 불필요한 주석 삭제

--- src/mockData/money.js
+++ src/mockData/money.js
@@ -7,14 +7,5 @@ const cash = [
   { money: 5000, count: 6 },
   { money: 10000, count: 3 },
 ];
-// const cash = {
-//   10: { money: 10, count: 10 },
-//   50: { money: 50, count: 2 },
-//   100: { money: 100, count: 5 },
-//   500: { money: 500, count: 7 },
-//   1000: { money: 1000, count: 8 },
-//   5000: { money: 5000, count: 6 },
-//   10000: { money: 10000, count: 3 },
-// };
 
 export default cash;
