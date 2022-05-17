c0ec7d6ee22b5a35af0ad33e3c6686ec39d99937
BB-choi <choibobae0809@gmail.com>
2022-05-17 18:29:00 +0900

    feat: ProcessArea 자동 스크롤

--- src/components/VendingMachine/DispenseArea/ProgressArea/ProgressArea.jsx
+++ src/components/VendingMachine/DispenseArea/ProgressArea/ProgressArea.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 
 import { ProcessContext } from "contexts/processContext";
 
@@ -10,11 +10,22 @@ const createKey = (value, idx) => value + idx;
 const ProgressArea = () => {
   const { process } = useContext(ProcessContext);
 
+  const processEndRef = useRef();
+
+  const scrollToBottom = () => {
+    processEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, []);
+
   return (
     <Wrapper>
       {process.map((curProcess, idx) => (
         <ProcessItem process={curProcess} key={createKey(curProcess, idx)} />
       ))}
+      <div ref={processEndRef} />
     </Wrapper>
   );
 };
