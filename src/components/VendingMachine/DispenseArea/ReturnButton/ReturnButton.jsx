15ea0a1072fb6a7dfc1eb429d8e9cac0cc1ff8c6
BB-choi <choibobae0809@gmail.com>
2022-05-18 11:02:08 +0900

    feat: 투입된 금액 없을시에 반환 버튼 동작하지 않도록 구현

--- src/components/VendingMachine/DispenseArea/ReturnButton/ReturnButton.jsx
+++ src/components/VendingMachine/DispenseArea/ReturnButton/ReturnButton.jsx
@@ -1,17 +1,28 @@
+import { useContext } from "react";
+
 import Button from "components/common/form/Button/Button";
+import { InsertedMoneyContext } from "contexts/moneyContext";
 import constants from "utils/constants";
 
 import returnButtonStyle from "./ReturnButton.styled";
 
 const { RETURN } = constants.BUTTON_NAME;
 
+const handleClickReturnButton = () => {
+  console.log("return button");
+};
+
 const ReturnButton = () => {
+  const { insertedMoney } = useContext(InsertedMoneyContext);
+
   return (
     <Button
       data={{
         name: RETURN,
       }}
       styles={returnButtonStyle}
+      onClick={handleClickReturnButton}
+      isDisabled={!insertedMoney.length}
     />
   );
 };
