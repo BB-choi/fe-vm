0fa3bc6d1524dce1e620a8bbbf78f396645c836b
BB-choi <choibobae0809@gmail.com>
2022-05-18 23:36:22 +0900

    feat: 소지금 초과시 금액 보정 템플릿 구현

--- src/components/VendingMachine/DispenseArea/InsertMoneyArea/InsertMoneyArea.jsx
+++ src/components/VendingMachine/DispenseArea/InsertMoneyArea/InsertMoneyArea.jsx
@@ -1,7 +1,10 @@
-import { useRef } from "react";
+import { useContext, useRef } from "react";
 
 import Button from "components/common/form/Button/Button";
 import Input from "components/common/form/Input/Input";
+import { SetInsertedMoneyContext } from "contexts/moneyContext";
+import { SetProgressContext } from "contexts/progressContext";
+import moneyHelper from "helper/moneyHelper";
 import constants from "utils/constants";
 
 import { Form, insertButtonStyle, inputStyle } from "./InsertMoneyArea.styled";
@@ -10,25 +13,42 @@ const {
   CURRENCY,
   BUTTON_NAME: { INSERT },
 } = constants;
+const { isWithinBaseMoney, computeTotalMoney } = moneyHelper;
 
 const MAX_INPUT_LENGTH = 5;
 const isOverMaxLength = (input) => {
   return input.length > MAX_INPUT_LENGTH;
 };
 
+const alertMessages = {
+  overMaxLength: `만 단위까지만 입력가능합니다.`,
+  overBaseMoney: `소지금을 초과하여 입력하였습니다.\n최대 금액이 투입됩니다.`,
+};
+
 const InsertMoneyArea = ({ value }) => {
   const inputRef = useRef(null);
+
+  const updateProgress = useContext(SetProgressContext);
+  const insertMoney = useContext(SetInsertedMoneyContext);
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
     if (isOverMaxLength(inputRef.current.value)) {
-      alert(`만 단위까지만 입력가능합니다.`);
+      alert(alertMessages.overMaxLength);
       inputRef.current.focus();
       return;
     }
 
     const inputNumber = Number(inputRef.current.value);
-    console.log(inputNumber);
+    const totalMoney =
+      computeTotalMoney(/* 여기에 지갑총금액--> 지갑 총 카운트 Context사용하면 될듯 */);
+    if (!isWithinBaseMoney(inputNumber, totalMoney)) {
+      alert(alertMessages.overBaseMoney);
+      // 금액 보정(최대금액으로 보정)
+      insertMoney(totalMoney);
+      updateProgress("insert", totalMoney);
+      // 지갑에서 모든 카운트 제거하는 로직 필요
+    }
   };
 
   return (
